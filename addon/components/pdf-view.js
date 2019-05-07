0c7f433a205e90469e1215c669e4106374876413
Michaël Dierick <MikiDi@users.noreply.github.com>
2019-05-07 10:20:01 +0200

    Update pdf-view.js
    Fix broken path

--- addon/components/pdf-view.js
+++ addon/components/pdf-view.js
@@ -10,7 +10,7 @@ export default Component.extend({
   filePath: '/ember-pdfjs-wrapper/pdfjs/web/compressed.tracemonkey-pldi-09.pdf',
   src: computed('filepath', {
     get(key) {
-      return `ember-pdfjs-viewer/pdfjs/web/viewer.html?file=${this.filePath}`;
+      return `ember-pdfjs-wrapper/pdfjs/web/viewer.html?file=${this.filePath}`;
     },
     set(key, value) {
       return value;
