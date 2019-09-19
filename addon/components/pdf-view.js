49cced3d7aa10ce2983eea36d7b1d9a16f0ef95e
Claire Lovisa <lovisa.claire@gmail.com>
2019-09-19 13:45:53 +0200

    Add prefix option

--- addon/components/pdf-view.js
+++ addon/components/pdf-view.js
@@ -10,7 +10,12 @@ export default Component.extend({
   filePath: '/ember-pdfjs-wrapper/pdfjs/web/compressed.tracemonkey-pldi-09.pdf',
   src: computed('filepath', {
     get(key) {
-      return `/ember-pdfjs-wrapper/pdfjs/web/viewer.html?file=${this.filePath}`;
+      if (this.prefix) {
+        return `${this.prefix}/ember-pdfjs-wrapper/pdfjs/web/viewer.html?file=${this.filePath}`;
+
+      } else {
+        return `/ember-pdfjs-wrapper/pdfjs/web/viewer.html?file=${this.filePath}`;
+      }
     },
     set(key, value) {
       return value;
