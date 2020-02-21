5f4d34d2d4ba42df2cf92e07e7465a3d13845a27
Michaël Dierick <MikiDi@users.noreply.github.com>
2020-02-21 11:31:27 +0100

    Fix typos

--- addon/components/pdf-view.js
+++ addon/components/pdf-view.js
@@ -10,10 +10,10 @@ export default Component.extend({
   filePath: '/ember-pdfjs-wrapper/pdfjs/web/compressed.tracemonkey-pldi-09.pdf',
 
   viewerUri: computed('prefix', function () {
-    const uri = '/ember-pdfjs-wrapper/pdfjs/web/viewer.html'
+    const uri = '/ember-pdfjs-wrapper/pdfjs/web/viewer.html';
     return this.prefix ? this.prefix + uri : uri;
   }),
-  src: computed('filePath', 'viewerUri' {
+  src: computed('filePath', 'viewerUri', {
     get(key) {
       return this.viewerUri + `?file=${encodeURIComponent(this.filePath)}`;
     },
